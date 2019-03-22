b861692d58e84fb49935bb2469b81645a8923cd2
victor l <leungvb@gmail.com>
2019-03-22 05:05:48 -0500

    table sorts by multicolumn, removed search, added pagination, icons and arrows for tables added. error handling for form submission

--- app.py
+++ app.py
@@ -16,18 +16,25 @@ db= SQLAlchemy(app)
 class Feature(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(75))
-    description =db.Column(db.String(75))
+    description = db.Column(db.String(75))
+    client = db.Column(db.String(10))
+    priority = db.Column(db.Integer)
+    date = db.Column(db.DATE)
 
     def __repr__(self):
         return self.title
 
-db.drop_all()
+
 db.create_all()  
 
 # saves form data to database
 def save_changes(feature, form):
     feature.title = form.title.data
     feature.description = form.description.data
+    feature.client = form.client.data
+    feature.priority = int(form.priority.data)
+    feature.date = form.date.data
+
     db.session.add(feature)
     db.session.commit()
 
@@ -36,7 +43,7 @@ def save_changes(feature, form):
 @app.route('/', methods=['GET'])
 def home():
     users=['User 1','User 2','User 3']
-    all_features = Feature.query.all()
+    all_features = Feature.query.order_by(Feature.client).all()
     print(all_features)
     print(type(all_features))
 
@@ -56,6 +63,16 @@ def feature():
         flash('Feature Successfully Added!')
         return redirect('/')        # change this to url_for
     return render_template('feature.html', form=form)
+
+# ------ Error Handlers ------- #
+
+@app.errorhandler(404)
+def pagenotfound(e):
+    return render_template('pagenotfound.html'), 404
+
+@app.errorhandler(500)
+def pagenotfound500(e):
+    return render_template('pagenotfound.html'), 500
 
 # ========= MAIN ======= #
 if __name__ == "__main__":
--- scripts/forms.py
+++ scripts/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, validators, TextAreaField, SelectField, SelectMultipleField
+from wtforms import StringField, validators, TextAreaField, SelectField, SelectMultipleField, IntegerField
 from wtforms.fields.html5 import DateField
 from wtforms.widgets import ListWidget, CheckboxInput
 
@@ -8,8 +8,9 @@ class MultiCheckboxField(SelectMultipleField):
     option_widget = CheckboxInput()
 
 class RequestForm(Form):
-    title = StringField('Title:', validators=[validators.required(), validators.Length(min=1, max=50)])
-    description = TextAreaField('Description:', validators=[validators.required(), validators.Length(min=1, max=500)])
-    client = SelectField('Client:', choices=[('A', 'Client A'), ('B', 'Client B'), ('C', 'Client C')])
-    date = DateField(id='datepick')
+    title = StringField('Title:', validators=[validators.required(), validators.Length(min=1, max=50, message='Invalid Length')])
+    description = TextAreaField('Description:', validators=[validators.required(), validators.Length(min=1, max=500), validators.DataRequired('Must Provide A Description')])
+    client = SelectField('Client:', choices=[('A', 'Client A'), ('B', 'Client B'), ('C', 'Client C')], validators= [validators.DataRequired('Pick a Client')])
+    priority = SelectField('Priority:', choices=[(str(x),y) for x,y in enumerate(range(1,11),1)])
+    date = DateField(id='datepick', validators= [validators.DataRequired('Pick a due date')])
     area = MultiCheckboxField('Product Area:', choices=[('1', 'Policies'), ('2', 'Billings'), ('3', 'Claims'), ('4', 'Reports')])
