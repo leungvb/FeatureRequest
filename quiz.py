a47138e79a045282af3613dfe94a13d82955eef4
leungvb <leungvb@users.noreply.github.com>
2019-04-03 12:38:21 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -2,7 +2,7 @@ from cryptography.fernet import Fernet
 
 key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
 
-# Oh no! The code is going over the edge! What are you going to do?
+'''Oh no! The code is going over the edge! What are you going to do?'''
 message = b'gAAAAABco998-PwCqnqduE_pmLw5nxR5ePW7npi1_fdUbQB5nC2jXWn' \
           b'_uL1GVrAJbekblmGjySI3y2u4GVM0R6rCbrWO1KIaK3R4dKDRD1yb4EENO' \
           b'NYuCx2lHoh9sptB_jOvxPyaFPzJtA92IzKEAf6TMm0Nw40LzYcg_ucvni-' \
@@ -17,6 +17,8 @@ def main():
 if __name__ == "__main__":
     main()
 
-# solution URL:
-# https://engineering-application.britecore.com/e/t2e119s3t/testImplementationEngineer
-# formatted for PEP8
+'''
+solution URL:
+https://engineering-application.britecore.com/e/t2e119s3t/testImplementationEngineer
+formatted for PEP8
+'''
